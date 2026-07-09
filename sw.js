9b0698cee96d6972a95669869023b0f9996d3bcd
youngha24 <youngha24@gmail.com>
2026-07-09 18:00:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 사주 ✦ 운세 — 오프라인 캐시 서비스워커 v1 (2026-07-06) */
-var CACHE="saju-v5";
+var CACHE="saju-v12";
 var ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png"];
 self.addEventListener("install",function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));
